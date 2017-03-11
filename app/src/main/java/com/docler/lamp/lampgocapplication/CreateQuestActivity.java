0dbab3859500d408bab0313a92269e5ad5247945
Miklós Boros <cherubmiki@gmail.com>
2017-03-11 15:07:13 +0100

    Cleaned up webview code and added Javascript support inside webview

--- app/src/main/java/com/docler/lamp/lampgocapplication/CreateQuestActivity.java
+++ app/src/main/java/com/docler/lamp/lampgocapplication/CreateQuestActivity.java
@@ -1,15 +1,10 @@
 package com.docler.lamp.lampgocapplication;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 public class CreateQuestActivity extends AppCompatActivity {
 
@@ -20,6 +15,11 @@ public class CreateQuestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_quest);
 
         WebView createQuestWebView = (WebView) findViewById(R.id.createQuestView);
+
+        WebSettings webSettings = createQuestWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
         createQuestWebView.loadUrl("http://www.agocs.hu");
     }
 }
