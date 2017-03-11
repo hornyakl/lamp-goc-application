88f65fe83b476172121713a18970371906114829
masterada <masterada@gmail.com>
2017-03-12 00:26:54 +0100

    Added gps

--- app/src/main/java/com/docler/lamp/lampgocapplication/RadarActivity.java
+++ app/src/main/java/com/docler/lamp/lampgocapplication/RadarActivity.java
@@ -1,13 +1,25 @@
 package com.docler.lamp.lampgocapplication;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.docler.lamp.lampgocapplication.Quest.Quest;
+import com.docler.lamp.lampgocapplication.sensorFusion.orientationProvider.OrientationProvider;
+import com.docler.lamp.lampgocapplication.sensorFusion.orientationProvider.RotationVectorProvider;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,11 +33,27 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class RadarActivity extends AppCompatActivity {
+
+    private static final int MIN_TIME_BW_UPDATES = 1000 * 5;
+    private static final int MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
+
     private JSONObject questList;
     private String questListJson;
     private String questListPath = "https://goc-lamp.tk/quest-list";
 
     private LampApplication application;
+
+    private RadarDrawView drawView;
+
+
+    private SensorManager sensorManager;
+    private LocationManager locationManager;
+
+    private ViewChangerSensorListener sensorListener;
+
+    private RadarLocationListener locationListener;
+
+    private OrientationProvider orientationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +62,25 @@ public class RadarActivity extends AppCompatActivity {
 
         application = (LampApplication) getApplication();
 
-        Canvas grid = new Canvas(Bitmap.createBitmap(h,w, Bitmap.Config.ARGB_8888));
-        grid. drawColor(Color.WHITE);
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.FILL);
-        grid.drawCircle(w/2, h/2 , w/2, paint);
 
-        AsyncTask<Void, Void, Void> mTask = new AsyncTask<Void, Void, Void>()
-        {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorListener = new ViewChangerSensorListener();
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new RadarLocationListener();
+
+        orientationProvider = new RotationVectorProvider(
+                sensorManager
+        );
+
+
+        FrameLayout frame = (FrameLayout) findViewById(R.id.radar_frame);
+
+        drawView = new RadarDrawView(this);
+
+        frame.addView(drawView);
+
+        AsyncTask<Void, Void, Void> mTask = new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... params) {
                 try {
@@ -63,20 +102,27 @@ public class RadarActivity extends AppCompatActivity {
                     Iterator<String> keys = questList.keys();
 
                     ArrayList<Quest> quests = new ArrayList<>(questList.length());
+                    drawView.clearPoints();
+
                     while (keys.hasNext()) {
                         String id = keys.next();
                         JSONObject questObject = questList.getJSONObject(id);
-                        quests.add(
-                            new Quest(
+                        Quest quest = new Quest(
                                 questObject.getInt("id"),
                                 questObject.getString("name"),
                                 questObject.getString("description"),
                                 questObject.getDouble("latitude"),
                                 questObject.getDouble("longitude"),
                                 questObject.getLong("experience_point")
-                            )
                         );
+
+                        quests.add(
+                                quest
+                        );
+
+                        drawView.addPoint(quest.getLatitude(), quest.getLongitude());
                     }
+
 
                     Toast.makeText(RadarActivity.this, "ejjjha", Toast.LENGTH_LONG);
                 } catch (JSONException e) {
@@ -109,6 +155,9 @@ public class RadarActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         application.stopViewChangeListen();
+        sensorManager.unregisterListener(sensorListener);
+        orientationProvider.stop();
+        locationManager.removeUpdates(locationListener);
 
         super.onPause();
     }
@@ -117,8 +166,116 @@ public class RadarActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        orientationProvider.start();
+
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        sensorManager.registerListener(sensorListener, sensor, SensorManager.SENSOR_DELAY_GAME);
+
+        registerLocationListener();
+
         application.startViewChangeListen(this);
     }
 
+    private void registerLocationListener() {
+        boolean isGPSEnabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        boolean isNetworkEnabled = locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+
+        // Add permission for gps and let user grant the permission
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    777
+            );
+
+            return;
+        }
+
+        if (isNetworkEnabled) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                    locationListener
+            );
+
+            locationListener.onLocationChanged(locationManager
+                    .getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            );
+        }
+
+        // if GPS Enabled get lat/long using GPS Services
+        if (isGPSEnabled) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                    locationListener
+            );
+
+            if (locationManager != null) {
+                locationListener.onLocationChanged(locationManager
+                        .getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                );
+            }
+        }
+    }
+
+    private class ViewChangerSensorListener implements SensorEventListener {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                float[] angles = new float[3];
+                orientationProvider.getEulerAngles(angles);
+                float angle = angles[0];
+
+                drawView.setAngle(angle);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+
+    private class RadarLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location == null)
+            {
+                return;
+            }
+
+            drawView.setLocation(location.getLatitude(), location.getLongitude());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
 
 }
